23a971c34931b99576770be2debbbcdf7479298e
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-07-14 07:57:56 +0900

    Improved error handling.

--- src/asset.rs
+++ src/asset.rs
@@ -2,6 +2,7 @@ use aide::axum::routing::get_with;
 use aide::axum::{ApiRouter, IntoApiResponse};
 use axum::body::Body;
 use axum::http::{HeaderValue, Response, StatusCode};
+use std::io::{Error, ErrorKind};
 use tracing::error;
 
 pub fn get_routes() -> Vec<(&'static str, &'static str, &'static str, &'static str)> {
@@ -66,25 +67,34 @@ pub fn create_router() -> ApiRouter {
 pub async fn get_handler(file_path: &str) -> impl IntoApiResponse {
     match read_file_and_detect_mime(file_path).await {
         Ok((contents, mime_type)) => build_response(contents, mime_type),
-        Err(err) => {
-            error!("Error handling file {}: {:?}", file_path, err);
-            build_error_response(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
-        }
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => build_error_response(StatusCode::NOT_FOUND, "File not found"),
+            _ => build_error_response(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
+        },
     }
 }
 
-async fn read_file_and_detect_mime(file_path: &str) -> Result<(Vec<u8>, String), std::io::Error> {
+async fn read_file_and_detect_mime(file_path: &str) -> Result<(Vec<u8>, String), Error> {
     let contents = tokio::fs::read(file_path).await?;
-    let mime_type = mime_guess::from_path(file_path).first_or_octet_stream().to_string();
+    let mime_type = mime_guess::from_path(file_path)
+        .first_or_octet_stream()
+        .to_string();
     Ok((contents, mime_type))
 }
 
 pub fn build_response(contents: Vec<u8>, mime_type: String) -> Response<Body> {
     Response::builder()
         .status(StatusCode::OK)
-        .header("Content-Type", HeaderValue::from_str(&mime_type).unwrap())
+        .header(
+            "Content-Type",
+            HeaderValue::from_str(&mime_type)
+                .unwrap_or_else(|_| HeaderValue::from_static("application/octet-stream")),
+        )
         .body(Body::from(contents))
-        .unwrap()
+        .unwrap_or_else(|e| {
+            error!("Failed to build response: {:?}", e);
+            build_error_response(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+        })
 }
 
 pub fn build_error_response(status: StatusCode, message: &str) -> Response<Body> {
@@ -92,5 +102,8 @@ pub fn build_error_response(status: StatusCode, message: &str) -> Response<Body>
         .status(status)
         .header("Content-Type", HeaderValue::from_static("text/plain"))
         .body(Body::from(message.to_string()))
-        .unwrap()
+        .unwrap_or_else(|e| {
+            error!("Failed to build error response: {:?}", e);
+            Response::new(Body::from("Internal server error"))
+        })
 }
